1f18d7799606e62421a5d9e2ead9cfac1927e53b
Yannick CADET <yannick.cadet@outlook.fr>
2022-11-09 16:44:36 +0100

    avec json dans branch

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Tu es fort 👍</h1>");
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    nom: "CADET",
+    prenom: "Yannick",
+    brand: "SANOFI",
+  });
 });
 
 app.listen(3900, () => {
